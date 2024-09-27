d4cb8a4bfc63bac1c1701bf4aff8d2dd138f6246
jaymonpike <jaymonpike@hotmail.com>
2024-09-26 18:31:54 -0600

    practiced formatting & escape char

--- python_strings.py
+++ python_strings.py
@@ -7,7 +7,7 @@ my_first_name = 'Jay'
 my_last_name = 'Pike'
 #       -set this equal to your last name
 #   - my_year_of_birth
-my_year_of_birth = '1936'
+my_year_of_birth = 1936
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 current_year = 2024
@@ -48,15 +48,21 @@ print(my_first_name * 6)
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
+print(my_first_name + ' ' + my_last_name +' was born in '+ str(my_year_of_birth))
+print(my_first_name, my_last_name, 'was born in', my_year_of_birth)
+print(f'{my_first_name} {my_last_name} was born in {my_year_of_birth}')
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
+age = current_year - my_year_of_birth
+print(f'{my_first_name} {my_last_name} is {age} years old')
 
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
-#       - possesive first name -birth year is- year of birth 
+#       - posessive first name -birth year is- year of birth 
+print(my_first_name + "'s birth year is " + str(my_year_of_birth))
+print(my_first_name + '\'s birth year is ' + str(my_year_of_birth))
 #       - tab last name current year
-
+print('\t' + my_last_name + str(current_year))
 
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
